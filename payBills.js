1f1a338a24794ea50c49e0debbefcfc6221e5955
Hanya Sherif <hanyasherif@Hanyas-MacBook-Pro.local>
2023-05-26 00:57:25 +0300

    Initial commit

---
+++ payBills.js
@@ -0,0 +1,64 @@
+const payButton1 = document.getElementById("payButton1").onclick = function(){
+    const thirdPartyRadio = document.getElementById("thirdParty1");
+    const bankRadio = document.getElementById("bank1");
+
+    if (thirdPartyRadio.checked) {
+      alert("Redirecting to third-party payment gateway...");
+    } else if (bankRadio.checked) {
+      alert("Payment successful!");
+    } else {
+      alert("Please select a payment method.");
+    }
+}
+
+const payButton2 = document.getElementById("payButton2").onclick = function(){
+    const thirdPartyRadio = document.getElementById("thirdParty2");
+    const bankRadio = document.getElementById("bank2");
+
+    if (thirdPartyRadio.checked) {
+      alert("Redirecting to third-party payment gateway...");
+    } else if (bankRadio.checked) {
+      alert("Payment successful!");
+    } else {
+      alert("Please select a payment method.");
+    }
+}
+
+const payButton3 = document.getElementById("payButton3").onclick = function(){
+    const thirdPartyRadio = document.getElementById("thirdParty3");
+    const bankRadio = document.getElementById("bank3");
+
+    if (thirdPartyRadio.checked) {
+      alert("Redirecting to third-party payment gateway...");
+    } else if (bankRadio.checked) {
+      alert("Payment successful!");
+    } else {
+      alert("Please select a payment method.");
+    }
+}
+
+const payButton4 = document.getElementById("payButton4").onclick = function(){
+    const thirdPartyRadio = document.getElementById("thirdParty4");
+    const bankRadio = document.getElementById("bank4");
+
+    if (thirdPartyRadio.checked) {
+      alert("Redirecting to third-party payment gateway...");
+    } else if (bankRadio.checked) {
+      alert("Payment successful!");
+    } else {
+      alert("Please select a payment method.");
+    }
+}
+
+const payButton5 = document.getElementById("payButton5").onclick = function(){
+    const thirdPartyRadio = document.getElementById("thirdParty5");
+    const bankRadio = document.getElementById("bank5");
+
+    if (thirdPartyRadio.checked) {
+      alert("Redirecting to third-party payment gateway...");
+    } else if (bankRadio.checked) {
+      alert("Payment successful!");
+    } else {
+      alert("Please select a payment method.");
+    }
+}
